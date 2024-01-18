b2b5acef9f5aff3ade0995154ae64a4a23158586
Querel matthieu <querelmatthieu@gmail.com>
2024-01-18 21:37:03 +0100

    authentification

--- App.tsx
+++ App.tsx
@@ -1,26 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import {Button, StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View ,Text} from 'react-native';
+import { NativeRouter,Routes, Route ,Link} from 'react-router-native';
+
+import Compte from './Components/Compte';
+import Authentification from './Components/Authentification';
+import CreactCompte from './Components/CreactCompte';
+import Erreur from './Components/Erreur';
 
 export default function App() {
-  const [count,setCount] = useState(0)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start workeding on your app!</Text>
-      
-      <Text>{count}</Text>
-      <Button title = 'test' onPress={()=> setCount(count + 1)}/>
-      <StatusBar style="auto" />
-    </View>
     
+    <NativeRouter>
+  
+        
+
+    <Routes>
+      <Route path="/" element={<Authentification />} />
+      <Route path="/CreactCompte" element={<CreactCompte />} />
+      <Route path="/Compte" element={<Compte />} />
+      <Route path="*" element={<Erreur />} />
+    </Routes>
+   
+  </NativeRouter>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
